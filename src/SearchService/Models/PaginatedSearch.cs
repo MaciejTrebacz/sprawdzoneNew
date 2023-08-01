87f8f8b81ad37e52b237571d97870d7e647ee509
MaciejTrebacz <maciekdmd90@gmail.com>
2023-08-01 03:39:08 +0200

    implementing searching and filtering methods

---
+++ src/SearchService/Models/PaginatedSearch.cs
@@ -0,0 +1,8 @@
+﻿namespace SearchService.Models;
+
+public class PaginatedSearch
+{
+    public IReadOnlyList<Motorcycle> ListOfMotorcycles { get; set; }
+    public int PageCount { get; set; }
+    public long TotalCount { get; set; }
+}
