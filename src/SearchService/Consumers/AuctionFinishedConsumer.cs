0677820316591809639c5e11078ac6fdecb58d75
MaciejTrebacz <maciekdmd90@gmail.com>
2023-08-09 00:54:23 +0200

    Setting up reverse proxy and consumers

---
+++ src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,28 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class AuctionFinishedConsumer :IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine($"Consuming finished Auction: {context.Message.AuctionId} for amount {context.Message.Amount}");
+
+        var auction = await DB.Find<Motorcycle>().OneAsync(context.Message.AuctionId);
+
+        if (auction == null) return;
+
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = (int)context.Message.Amount;
+        }
+
+        auction.Status = "Finished";
+
+        await auction.SaveAsync();
+    }
+}
---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine($"Consuming bid placed of ID: {context.Message.AuctionId} for amount {context.Message.Amount}");
+
+        var motorcycle = await DB.Find<Motorcycle>().OneAsync(context.Message.AuctionId) ;
+
+
+        if (context.Message.BidStatus == BidStatus.Accepted
+            && context.Message.Amount > motorcycle.CurrentHighBid)
+        {
+            motorcycle.CurrentHighBid = context.Message.Amount;
+            await motorcycle.SaveAsync();
+        }
+    }
+}
